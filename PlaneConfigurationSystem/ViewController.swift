30f7d1bed46665896418bd9399842e911adf5608
Kirill Somov <gornomirov661@gmail.com>
2025-01-16 14:52:46 +0300

    Initial Commit

---
+++ PlaneConfigurationSystem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlaneConfigurationSystem
+//
+//  Created by Сомов Кирилл on 16.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
